81b1001cc5590cf1b304868e47bff3eefb23877d
Batuhan Özhan <batuhanozhan00@gmail.com>
2024-06-20 00:05:19 +0300

    Initial Commit

---
+++ twitter-clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter-clone
+//
+//  Created by Batuhan Özhan on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
